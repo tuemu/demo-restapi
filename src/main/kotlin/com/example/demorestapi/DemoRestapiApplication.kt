a56f9b5b5beedfb03b8d6ac6ca404c77344bf629
Tomohiro Uemura <t.uemura618@gmail.com>
2020-01-22 22:37:05 +0800

    First commit

---
+++ src/main/kotlin/com/example/demorestapi/DemoRestapiApplication.kt
@@ -0,0 +1,11 @@
+package com.example.demorestapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DemoRestapiApplication
+
+fun main(args: Array<String>) {
+	runApplication<DemoRestapiApplication>(*args)
+}
